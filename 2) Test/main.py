1a00a70b303bd4e3a5e5188b93b96542faa1092a
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2025-12-07 14:35:06 +0500

    FastAPI Parameters | Path, Query, and Body Parameters for AI Apps

--- 2) Test/main.py
+++ 2) Test/main.py
@@ -2,15 +2,44 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-@app.post("/")
-def read_root():
-    return {"Hello": "Post"}
-@app.delete("/")
-def read_root():
-    return {"Hello": "Delete"}
+@app.get("/{item_id}/{name}")
+def read_root(item_id: int, name: str):
+    try:
+        return {
+            "data": {
+                "item_id": item_id,
+                "name": name
+            },
+            "status": 200,
+        }
+    except Exception as e:
+        return {
+            "message": str(e),
+            "status": 500,
+        }
+
+@app.post("/user")
+def read_root(name: str, age: int):
+    try:
+        return {
+            "data": "User created with name: {} and age: {}".format(name, age),
+            "status": 200,
+        }
+    except Exception as e:
+        return {
+            "message": str(e),
+            "status": 500,
+        }
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str = None
+
+@app.post("/items")
+def create_item(item: Item):
+    return {"item_name": item.name, "item_price": item.price}
 
 
 
