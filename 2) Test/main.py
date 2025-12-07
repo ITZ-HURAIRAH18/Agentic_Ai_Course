01efcdcad7c8a2c3106c7f123a2e792d31347840
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2025-12-07 11:43:46 +0500

    FastAPI Routing Deep Dive

--- 2) Test/main.py
+++ 2) Test/main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+@app.post("/")
+def read_root():
+    return {"Hello": "Post"}
+@app.delete("/")
+def read_root():
+    return {"Hello": "Delete"}
+
+
+
+
+# poetry run uvicorn main:app --reload
